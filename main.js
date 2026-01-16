f42207f6b5cc1ae6116d8a71163be3a195d7b062
Subodh Shah <shahsubodh166@gmail.com>
2026-01-16 10:43:15 +0545

    add boiler_plate

---
+++ main.js
@@ -0,0 +1,6 @@
+function knightMoves(start, end) {
+	console.log("Working just fine");
+	console.log(`Congrats Knight just got there in ${stepsCounter} steps`);
+	console.log(`${path1}->${path2}->${path3}......`);
+	return 0;
+}
