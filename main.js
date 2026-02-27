0d103f1dc49c0aaf5170fafae9c54f9ce551bfc8
Subodh Shah <shahsubodh166@gmail.com>
2026-02-27 22:08:09 +0545

    closing commit

--- main.js
+++ main.js
@@ -1,6 +1,80 @@
-function knightMoves(start, end) {
-	console.log("Working just fine");
-	console.log(`Congrats Knight just got there in ${stepsCounter} steps`);
-	console.log(`${path1}->${path2}->${path3}......`);
-	return 0;
+class Queue {
+	constructor() {
+		this.queue = [];
+	}
+	
+	enqueue(node) {
+		this.queue.push(node);
+	}
+	
+	dequeue() {
+		return this.queue.shift();
+	}
+	
+	size() {
+		return this.queue.length;
+	}
 }
+
+class Node {
+	//coordinates as array and parent as another node object or null for start position
+	constructor(coordinates, parent = null) {
+		this.coordinates = coordinates;
+		this.parent = parent;
+	}
+}
+
+function buildPath(node) {
+	let path = [];
+	while(node !== null) {
+		path.unshift(node.coordinates);
+		node = node.parent;
+	}
+	return path;
+}
+
+function knightMoves(x, y) {
+	let moves = [[1,2], [1,-2], [-1,2], [-1,-2], 
+				 [2,1], [2,-1], [-2,1], [-2,-1]];
+	return moves.map(([dx, dy]) => [x + dx, y + dy])
+				.filter(([newX, newY]) => newX>=0 && newX<8 && newY>=0 && newY<8);
+}
+
+function knightTravails(start, destination) {
+	let queue = new Queue();
+	let visited = Array(8).fill().map(() => Array(8).fill(false));
+	let startNode = new Node(start);
+	queue.enqueue(startNode);
+	visited[startNode.coordinates[0]][startNode.coordinates[1]] = true;
+	
+	while(queue.size() > 0) {
+		let current = queue.dequeue();
+		if(current.coordinates[0] === destination[0] &&
+		current.coordinates[1] === destination[1]) {
+			return buildPath(current);
+		}
+		
+		let nextMoves = knightMoves(current.coordinates[0], current.coordinates[1]);
+		for (let position of nextMoves) {
+			let possibleNextNode = new Node(position, current);
+			if(visited[possibleNextNode.coordinates[0]]
+				[possibleNextNode.coordinates[1]] === false) {
+						  queue.enqueue(possibleNextNode);
+						  visited[possibleNextNode.coordinates[0]]
+								[possibleNextNode.coordinates[1]] = true;
+			}
+		}
+	}
+}
+
+let knightPath = knightTravails([1,7], [0,3]);
+console.log(knightPath);
+let knigthPathString = '';
+knightPath.forEach((position, index) => {
+	if(index !== knightPath.length - 1) {
+		knigthPathString += `[${position}] -> `;
+	} else {
+		knigthPathString += `[${position}]`;
+	}
+});
+console.log(knigthPathString);
